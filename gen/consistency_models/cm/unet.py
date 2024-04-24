39860e60b5ece52e3e61a2bb97d6cee1debd1222
tristan.j.peat@gmail.com <tristan.j.peat@gmail.com>
2024-04-24 14:46:40 -0400

    new attention

--- gen/consistency_models/cm/unet.py
+++ gen/consistency_models/cm/unet.py
@@ -18,7 +18,7 @@ from cm.nn import (
     timestep_embedding,
 )
 
-
+# merge fix
 class AttentionPool2d(nn.Module):
     """
     Adapted from CLIP: https://github.com/openai/CLIP/blob/main/clip/model.py
